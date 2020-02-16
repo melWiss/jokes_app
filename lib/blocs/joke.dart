dfdd86f65a3b227af21c5a327adba9619bd91090
Oussama Maatallah <oussamamaatallah01@gmail.com>
2020-02-16 19:20:40 +0100

    fixed some problems in jokes cards.

--- lib/blocs/joke.dart
+++ lib/blocs/joke.dart
@@ -10,33 +10,35 @@ class JokesApi {
 
   JokesApi() {
     //_streamController.add(_jokes);
-    /*for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) {
       getJokes().then((value) {
         _jokes.add(value);
       }).whenComplete(() => _jokesSink.add(_jokes));
-    }*/
-    Timer.periodic(Duration(milliseconds: 500), (timer) {
-      getJokes().then((value) {
-        _jokes.add(value);
-      }).whenComplete(() => _jokesSink.add(_jokes));
-    });
+    }
   }
 
   swipeRight() {
-    /*getJokes().then((value) {
+    getJokes().then((value) {
+      if (value != "NULL") {
         _jokes.add(value);
         _jokesSink.add(_jokes);
-      });*/
+      }
+    });
   }
 
   Future getJokes() async {
-    var response =
-        await http.get("https://joke3.p.rapidapi.com/v1/joke", headers: {
-      "x-rapidapi-host": "joke3.p.rapidapi.com",
-      "x-rapidapi-key": "82f039ffd4msh4f43b5938417b2fp11abd7jsne56264a66ebb"
-    });
-    var result = jsonDecode(response.body);
-    return result['content'];
+    var result;
+    try {
+      var response =
+          await http.get("https://joke3.p.rapidapi.com/v1/joke", headers: {
+        "x-rapidapi-host": "joke3.p.rapidapi.com",
+        "x-rapidapi-key": "82f039ffd4msh4f43b5938417b2fp11abd7jsne56264a66ebb"
+      });
+      result = jsonDecode(response.body)['content'];
+    } catch (e) {
+      result = "NULL";
+    }
+    return result;
   }
 
   dispose() {
